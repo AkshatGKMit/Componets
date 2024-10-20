3f9ef24351c47276ee44638992422a2b77f32959
AkshatGKMit <akshat@gkmit.co>
2024-10-20 22:00:03 +0530

    feat: Add theme switcher in settings

--- src/screens/settings/Settings.tsx
+++ src/screens/settings/Settings.tsx
@@ -1,11 +1,26 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, Switch } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
 import globalStyles from '@src/styles/global';
+import ThemeContext from '@src/contexts/ThemeContext';
 
 const Settings = () => {
+  const { isDark, changeTheme } = useContext(ThemeContext);
+  const [themeSwitcher, setThemeSwitcher] = useState(false);
+
+  useEffect(() => {
+    setThemeSwitcher(isDark);
+  }, [isDark]);
+
+  function onSwitchTheme(val: boolean) {
+    changeTheme(isDark ? 'light' : 'dark');
+  }
+
   return (
     <View style={[globalStyles().screen, globalStyles().flexColumn]}>
-      <Text>Settings</Text>
+      <Switch
+        value={themeSwitcher}
+        onValueChange={onSwitchTheme}
+      />
     </View>
   );
 };
